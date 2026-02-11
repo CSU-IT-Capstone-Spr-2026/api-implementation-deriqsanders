e759ca74f3a3d9176bdfa3af18545df942e5e2cc
deriqsanders <80486623+deriqsanders@users.noreply.github.com>
2026-02-10 21:10:43 -0500

    Refactor previous_comic function for clarity
    Fix indentation and remove redundant line in previous_comic function.

--- app.py
+++ app.py
@@ -67,10 +67,10 @@ def show_comic(comic_num):
 
 def previous_comic(comic_num):
 
-response = requests.get(f"{XKCD_BASE_URL}/{comic_num}/info.0.json")
+    comic_num - 1
 
-    try:
-        comic_num - 1
+    try: 
+        response = requests.get(f"{XKCD_BASE_URL}/{comic_num}/info.0.json")
         
         if comic_num == 0:
             return render_r template('index.html', comic=None,
